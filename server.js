6bfffe2c8a1cd747b2b3af3581b9159e40fc5265
Ethan C <ethan.cho1826@gmail.com>
2021-11-01 19:04:31 -0500

    frontend logic to display a button if user is logged in

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+const passport = require('passport');
 // const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
@@ -33,6 +34,8 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(routes);
 
