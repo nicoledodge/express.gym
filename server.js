a571dd7a981bdc0090a9fc9b40d107dd743beb26
nicoledodge <nicoledodge5@gmail.com>
2021-11-04 14:45:49 -0500

    lol phillip

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3001;
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
